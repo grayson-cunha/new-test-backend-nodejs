e994cc1c3e68e09a737baf26d2d314d39453c1b9
Grayson Cunha <graysonsilva93@gmail.com>
2023-08-01 10:16:09 -0300

    feat(catalog): upload catalog for s3 bucket

--- src/app/consumers/handlers/generate-catalog-json-handler.js
+++ src/app/consumers/handlers/generate-catalog-json-handler.js
@@ -1,9 +1,12 @@
 import catalogService from '../../services/catalog.service';
+import s3Service from '../../services/s3.service';
 
 export default async function generateCatalogJsonHandler(messages) {
   for (const message of messages) {
     const messageBody = JSON.parse(message.Body);
 
     const catalog = await catalogService.generateCatalog(messageBody.ownerId);
+
+    await s3Service.uploadFile(`${messageBody.ownerId}.json`, catalog);
   }
 }
